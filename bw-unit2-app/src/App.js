4c6172a151f2f354fb3c15cdb433a7ed652e467b
rae-glazier <glazier.home.life@gmail.com>
2020-02-01 12:25:52 -0600

    basic form layed out

--- bw-unit2-app/src/App.js
+++ bw-unit2-app/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//components
+import RegisterForm  from './components/registerForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h1>Essentialism 2020</h1>
+      <p>In a world with everything shouting for attention, the disciplined pursuit of less has never been more needed</p>
+      <br/>
+      <p>Enter Essentialism: doing less but better so that you can make the highest possible contribution</p>
+
+      <div>
+        <RegisterForm />
+      </div>
+
     </div>
   );
 }
